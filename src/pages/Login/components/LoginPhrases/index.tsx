2ccd8d820131f061beb22be644237117fa281ded
Leticia Leal <leticialealp@hotmail.com>
2023-05-22 22:31:11 -0300

    fix: corrige erro de import

--- src/pages/Login/components/LoginPhrases/index.tsx
+++ src/pages/Login/components/LoginPhrases/index.tsx
@@ -1,27 +1,35 @@
-import { Box, Typography } from "@mui/material"
-import { PhrasesProps } from "../.."
+import { Box, Typography } from '@mui/material';
+
+import { PhrasesProps } from '../../types/PhrasesProps';
 
 interface LoginPhrasesProps {
-  phrases: PhrasesProps[]
+	phrases: PhrasesProps[];
 }
 
-const LoginPhrases: React.FC<LoginPhrasesProps> = ({phrases}) => {
-    return (
-        <Box sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignSelf: 'flex-start'
-        }}
-        gap={2}
-        >
-          {phrases.map(({ phrase }) => {
-            return(
-              <Typography variant="h6" color={'white'} component={'p'}>{phrase}.</Typography>
-            )
-          })}
-        </Box>
-    )
-}
+const LoginPhrases: React.FC<LoginPhrasesProps> = ({ phrases }) => {
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignSelf: 'flex-start',
+			}}
+			gap={2}
+		>
+			{phrases.map(({ phrase }) => {
+				return (
+					<Typography
+						key={phrase}
+						variant="h6"
+						color={'white'}
+						component={'p'}
+					>
+						{phrase}.
+					</Typography>
+				);
+			})}
+		</Box>
+	);
+};
 
 export default LoginPhrases;
-
